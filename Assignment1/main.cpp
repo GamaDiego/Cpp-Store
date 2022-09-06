4cd94657dee85ec41a7fbeb3812d3aa793058e53
Diego Galvan <dgalvan@trevecca.edu>
2022-09-06 11:00:21 -0500

    Initial Commit

---
+++ Assignment1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Diego Galván Magallón on 9/6/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
